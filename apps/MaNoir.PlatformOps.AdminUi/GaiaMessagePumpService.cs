26af5934478718614ef945a8232ae55c99f8cffc
Michael CARBENAY <michael@carbenay.info>
2026-09-13 18:16:00 +0200

    Not trying to connect to NATS before it is deployed

--- apps/MaNoir.PlatformOps.AdminUi/GaiaMessagePumpService.cs
+++ apps/MaNoir.PlatformOps.AdminUi/GaiaMessagePumpService.cs
@@ -10,15 +10,23 @@ public sealed class GaiaMessagePumpService : BackgroundService
 {
 	private readonly GaiaMessageRouter _messageRouter;
 	private readonly GaiaAgentRuntime _runtime;
+	private readonly GaiaOperationsService _gaia;
 
-	public GaiaMessagePumpService(GaiaMessageRouter messageRouter, GaiaAgentRuntime runtime)
+	public GaiaMessagePumpService(GaiaMessageRouter messageRouter, GaiaAgentRuntime runtime, GaiaOperationsService gaia)
 	{
 		_messageRouter = messageRouter ?? throw new ArgumentNullException(nameof(messageRouter));
 		_runtime = runtime ?? throw new ArgumentNullException(nameof(runtime));
+		_gaia = gaia ?? throw new ArgumentNullException(nameof(gaia));
 	}
 
 	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 	{
+		while (!_gaia.IsMinimumVitalReady && !stoppingToken.IsCancellationRequested)
+			await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+
+		if (stoppingToken.IsCancellationRequested)
+			return;
+
 		_runtime.ReportTopicsSubscribed();
 
 		Task listenerTask = Task.Run(() => NatsInterprocessListener.Run(_runtime.MessageTopics, _messageRouter.HandleMessage), CancellationToken.None);
